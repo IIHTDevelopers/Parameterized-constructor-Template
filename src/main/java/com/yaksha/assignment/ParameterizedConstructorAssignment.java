13cacefa2985eb18e41e0873944604ecab1fc42f
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-31 07:58:16 +0530

    Ameet - Update ParameterizedConstructorAssignment.java
    Update ParameterizedConstructorAssignment.java for template

--- src/main/java/com/yaksha/assignment/ParameterizedConstructorAssignment.java
+++ src/main/java/com/yaksha/assignment/ParameterizedConstructorAssignment.java
@@ -1,19 +1,7 @@
 package com.yaksha.assignment;
 
-class Person {
- String name;
-
- // Parameterized constructor that accepts a name argument
- public Person(String name) {
-     this.name = name;  // Initializing the name with the passed argument
- }
-}
-
 public class ParameterizedConstructorAssignment {
  public static void main(String[] args) {
-     // Task 2: Use Parameterized Constructor in the Main Method
-     
-     Person person = new Person("Alice");  // Creating an object of Person using the parameterized constructor
-     System.out.println("Person's name: " + person.name);  // Printing the initialized name
+     // write your logic here
  }
 }
